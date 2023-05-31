32909f136d93df8374ec93f4ddd5121efa58194c
Lucas Siqueira <lucassiq_nava@prestadorliberty.com.br>
2023-05-31 14:41:19 -0300

    feat: finished code

--- App.tsx
+++ App.tsx
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, Text, View } from 'react-native';
+import * as LocalAuthentication from 'expo-local-authentication'
+
+import { styles } from './styles'
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [isAuthenticate, setIsAuthenticate] = useState(false);
+
+  async function verifyAvailableAuthentication() {
+    // metodo para ver se o dispositivo é compatível com a autenticação local
+    const compatible = await LocalAuthentication.hasHardwareAsync();
+
+    // metodo para ver os tipos de autenticação local disponíveis para uso
+    const types = await LocalAuthentication.supportedAuthenticationTypesAsync();
+  }
+
+  async function handleAuthentication() {
+    // metodo que verifica se existe algum tipo de autenticação cadastrado no dispositivo
+    const isBiometricEnrolled = await LocalAuthentication.isEnrolledAsync();
+
+    if (!isBiometricEnrolled) {
+      return Alert.alert(
+        'Login',
+        'Nenhuma biometria encontrada. Por favor cadastre no dispositivo!'
+      )
+    }
+
+    // metodo que chama a autenticação local para validar o usuário
+    const auth = await LocalAuthentication.authenticateAsync({
+      promptMessage: 'Login com biometrica',
+      fallbackLabel: 'Biometria não reconhecida',
+    });
+
+    setIsAuthenticate(auth.success);
+  }
+
+  useEffect(() => {
+    verifyAvailableAuthentication();
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>
+        Usuário Conectado: {isAuthenticate ? 'Sim' : 'Não'}
+      </Text>
+
+      <Button
+        title="Entrar"
+        onPress={handleAuthentication}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
